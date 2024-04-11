922deb8a45ecc6adffe004051fe713a090c82474
Manthan Sharma <mayank9870866130@gmail.com>
2024-04-11 16:48:45 +0530

    add login page ui

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,13 +1,116 @@
+import {
+  Button,
+  Card,
+  Checkbox,
+  Form,
+  Input,
+  Layout,
+  Space,
+  Typography,
+} from "antd";
+import { LockFilled, UserOutlined, LockOutlined } from "@ant-design/icons";
+
+const { Paragraph } = Typography;
+
 const LoginPage = () => {
   return (
     <>
-      <h1>Sign In</h1>
-      <input type="text" placeholder="Username" />
-      <input type="text" placeholder="Password" />
-      <button>Log In</button>
-      <label htmlFor="remember-me">Remember me</label>
-      <input type="checkbox" name="" id="remember-me" />
-      <a href="#">Forgot Password</a>
+      <Layout
+        style={{
+          height: "100vh",
+          display: "grid",
+          placeItems: "center",
+        }}
+      >
+        <Space direction="vertical" align="center" size={"small"}>
+          <Layout.Content>
+            <Paragraph
+              italic={true}
+              style={{
+                color: "#F65F42",
+              }}
+            >
+              E-Haat
+            </Paragraph>
+          </Layout.Content>
+
+          <Card
+            title={
+              <Space
+                style={{
+                  width: "100%",
+                  fontSize: 16,
+                  justifyContent: "center",
+                }}
+              >
+                <LockFilled />
+                Sign In
+              </Space>
+            }
+            bordered={false}
+            style={{
+              width: 300,
+            }}
+          >
+            <Form initialValues={{ remember: true }}>
+              <Form.Item
+                name="email"
+                rules={[
+                  {
+                    required: true,
+                    message: "Please enter your email",
+                  },
+                  {
+                    type: "email",
+                    message: "invalid email",
+                  },
+                ]}
+              >
+                <Input prefix={<UserOutlined />} placeholder="Email" />
+              </Form.Item>
+
+              <Form.Item
+                name="password"
+                rules={[
+                  {
+                    required: true,
+                    message: "Please enter your password",
+                  },
+                ]}
+              >
+                <Input.Password
+                  prefix={<LockOutlined />}
+                  placeholder="Password"
+                />
+              </Form.Item>
+
+              <Space style={{ marginBottom: 10 }}>
+                <Form.Item
+                  name="remember"
+                  valuePropName="checked"
+                  style={{ height: 10 }}
+                >
+                  <Checkbox>Remember me</Checkbox>
+                </Form.Item>
+
+                <a href="" id="login-form-forgot">
+                  Forgot Password
+                </a>
+              </Space>
+
+              <Form.Item>
+                <Button
+                  type="primary"
+                  htmlType="submit"
+                  style={{ width: "100%" }}
+                >
+                  Log In
+                </Button>
+              </Form.Item>
+            </Form>
+          </Card>
+        </Space>
+      </Layout>
     </>
   );
 };
